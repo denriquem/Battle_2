eba6c7b9fe1fe6994670c226a202d40cb5cdc026
Daniel <demartinezgatell@gmail.com>
2020-03-15 17:50:07 +0000

    refactor: web helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,7 @@
+
 feature 'End names' do 
     scenario 'submitting names' do 
-        visit('/')
-        fill_in :player_1_name, with: 'Daniel'
-        fill_in :player_2_name, with: 'Schmitens'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Daniel vs. Schmitens'
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 ENV['RACK_ENV'] = 'test'
 
+
 #require our sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb') 
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'features/web_helpers'
 
 Capybara.app = Battle
 
